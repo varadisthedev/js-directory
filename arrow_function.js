0b603bbe33b2d1eb0a479c13c2ee3727d849edec
varadisthedev <varadisthedev@gmail.com>
2025-08-20 16:42:09 +0530

    added arrow function and object manipulation

---
+++ arrow_function.js
@@ -0,0 +1,16 @@
+let hello = () => {
+  return "Hello World!";
+} 
+
+let greet = () => "Hello World!"; 
+
+console.log(hello());
+console.log(greet());
+
+const user_profile={
+    name:"Varad",
+    age:19,
+    address:"nagpur, india "
+};
+
+console.log(user_profile.age)
--- heyya.js
+++
@@ -1,8 +0,0 @@
-let hello = () => {
-  return "Hello World!";
-} 
-
-let greet = () => "Hello World!"; 
-
-console.log(hello());
-console.log(greet())
