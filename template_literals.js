6be3046bb4c3af49bb0cabdb052866cc41a0c120
varadisthedev <varadisthedev@gmail.com>
2025-08-26 01:17:04 +0530

    worked with template literals, how it is the similar to  f-string but better

---
+++ template_literals.js
@@ -0,0 +1,19 @@
+// string interpolation in js 
+const name="varad";
+console.log(
+    `hello there! my name is ${name}
+    and my fav food is panner Butter masala`
+);
+
+
+const data=
+`multi line 
+strings 
+are posssible using template literals`;
+console.log(data);
+
+// exprssion evaluation using template literals
+const num1=12;const num2=12;
+console.log(`is num1 greater than num2? ${num1>num2}`);
+
+//tagged templates
