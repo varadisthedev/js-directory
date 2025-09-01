daf433e0aef0740db6ce695b50e9e4ec103d2118
varadisthedev <varadisthedev@gmail.com>
2025-09-02 01:28:09 +0530

    feat(dom): practiced with text,css and todo list

---
+++ js-dom-miniwork/script.js
@@ -0,0 +1,49 @@
+// fetching the elements via tha DOM (Document Object Model)
+
+
+
+//updating the paragraph usign a button
+const p=document.getElementById("para-core")
+const button_var=document.getElementById("button-core")
+
+button_var.addEventListener("click",()=>{p.textContent="you did it."})
+
+//changing the color of a div usign a button 
+const bgcolor=document.getElementById("container")
+const bgbutton=document.getElementById("container-button")
+const changeColor=()=>{ // declaring the fucntion after assignment makes hoisting issues go away
+    bgcolor.style.backgroundColor="pink"
+}
+bgbutton.addEventListener("click",changeColor)
+
+//dynamically updating text from input box
+const display=document.getElementById("namedisplay")
+const input=document.getElementById("nameinput")
+
+const changeinput=()=>{
+    display.textContent=input.value
+}   
+input.addEventListener("input",changeinput)
+
+//adding itesm to the list (core of TODO)
+const list=document.getElementById("list")
+const taskInput=document.getElementById("taskInput")
+const button=document.getElementById("taskbutton")
+const item=document.createElement("li")
+
+const adder=()=>{
+    const item=document.createElement("li")
+    item.textContent = taskInput.value;
+    list.appendChild(item)
+    //making it so that list item deletes when tapped on them 
+    li.addEventListener("click",function(){
+        li.remove();
+    })
+    
+    taskInput.value=""
+}
+
+button.addEventListener("click",adder)
+
+
+
